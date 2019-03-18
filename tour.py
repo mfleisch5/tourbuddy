d168ed5bcb361c0a5d6816357e6eb991f199e5fa
Michael Fleischmann <mfleisch50@gmail.com>
2019-03-18 15:22:42 -0400

    Filter results

--- tour.py
+++ tour.py
@@ -2,26 +2,52 @@ import utils, requests, json
 
 API = utils.api()
 
-class Tour:
-    def __init__(self, location):
-        self.location = location
 
-    def geolocate(self):
+class Tour:
+    def __init__(self, location=None, json_data=None):
+        self.location = None
+        self.places = None
+        self.initialize(location, json_data)
+
+
+    def get_locality(self):
         query = 'https://maps.googleapis.com/maps/api/' \
                 'geocode/json?address={}&key={}'.format(self.location.replace(' ', '+'), API)
-        geocode = tuple(requests.get(query).json()['results'][0]['geometry']['location'].values())
-        return geocode
-
+        address_components = requests.get(query).json()['results'][0]['address_components']
+        locality = next(comp for comp in address_components if 'locality' in comp['types'])['short_name'].strip()
+        return locality
 
     def get_places(self):
-        location_str = ','.join([str(point) for point in self.geolocate()])
-        query = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?' \
-                'location={}&keyword=point+of+interest&radius=2000&key={}'.format(location_str, API)
-        print(query)
-        places = requests.get(query).json()
-        print(json.dumps(places, indent=4))
+        locality = self.get_locality().lower().replace(' ', '+')
+        query = 'https://maps.googleapis.com/maps/api/place/textsearch/json?' \
+                'query={}+point+of+interest&language=en&key={}'.format(locality, API)
+        return requests.get(query).json()['results']
+
+    def dump_places(self, file):
+        compressed = {'location': self.location, 'places': self.get_places()}
+        with open(file, 'w') as jf:
+            json.dump(compressed, jf, indent=4)
+
+    def filter_destinations(self):
+        self.places.sort(key=lambda dest: -dest['user_ratings_total'])
+        del self.places[20:]
+        print(*[(dest['name'], dest['types'], dest['user_ratings_total']) for dest in self.places], sep='\n')
 
 
+    def initialize(self, location, dest_json):
+        if bool(location) == bool(dest_json):
+            raise ValueError("Input must be either location string or json destination")
+        if location:
+            self.location = location
+            self.places = self.get_places()
+        else:
+            with open(dest_json, 'r') as dj:
+                data = json.load(dj)
+                self.location = data['location']
+                self.places = data['places']
+        self.filter_destinations()
 
-tour1 = Tour('23 Worcester Sq, 02118')
-tour1.get_places()
+
+tour1 = Tour(json_data='turin.json')
+#tour1.filter_destinations()
+tour2.filter_destinations()
